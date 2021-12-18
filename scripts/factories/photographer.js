95a560fbdf65ae8f57716b87978d0daa97c242ea
Laury-THIALLIER <tig252@gmail.com>
2021-12-18 16:10:42 +0100

    Functional index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    const pLocation = document.createElement("p");
+    const pTagline = document.createElement("p");
+    const pPrice = document.createElement("p");
+    h2.textContent = name;
+    pLocation.textContent = city + ", " + country;
+    pTagline.textContent = tagline;
+    pPrice.textContent = price + "€/jour";
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(pLocation);
+    article.appendChild(pTagline);
+    article.appendChild(pPrice);
+    return article;
+  }
+  return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
